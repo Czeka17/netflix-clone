d586b5a6f169ec64441b789e83d7046a9eed25eb
Jakub <jczekanski123@gmail.com>
2023-05-10 21:19:21 +0200

    fixed bug with searching

--- pages/search.js
+++ pages/search.js
@@ -31,13 +31,15 @@ function Search({ popularMovies, topRatedMovies, upcomingMovies }){
   
     const router = useRouter();
   const searchQuery = router.query.q; 
-  let filteredMovies
-  if (searchQuery) {
     const searchResults = fuse.search(searchQuery);
-    filteredMovies = searchResults.map(result => result.item);
+    const filteredMovies = searchResults.map(result => result.item);
+let sortedMovies
+  if(searchQuery){
+     sortedMovies = filteredMovies.sort((a, b) => a.title.localeCompare(b.title));
   }
-
-const sortedMovies = filteredMovies.sort((a, b) => a.title.localeCompare(b.title));
+  if(searchQuery === ''){
+    sortedMovies = uniqueMovies.sort((a, b) => a.title.localeCompare(b.title));
+  }
 
 
 
@@ -81,16 +83,16 @@ const handleMovieHover = (movie) => {
     
       return (
         <>
-        <div className="flex flex-col items-center justify-center pt-40 text-white">
+        <div className="flex flex-col items-center justify-center pt-24 text-white">
   <h1 className="text-xl">Search Results for <span className="font-bold">{searchQuery}</span></h1>
-  <div className="grid grid-cols-1 px-4 mx-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-4">
+  <div className="grid grid-cols-1 px-4 mx-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
     {sortedMovies.map((movie, index) => (
       <div key={movie.id} className="p-2 my-24 md:my-10 mx-10 md:mx-1 h-[10rem]" aria-label={`search result`}>
         <Movie movie={movie} index={index} isHovering={isHovering} selectedMovie={selectedMovie} watchlist={newWatchlist} handleMovieClick={handleMovieClick} handleMovieHover={handleMovieHover} handleMouseLeave={handleMouseLeave} />
       </div>
     ))}
   </div>
-  {filteredMovies.length === 0 && <p className="text-white text-2xl text-center flex justify-center items-center p-6">Cant find movie that you are looking for.</p>}
+  {filteredMovies.length === 0 && searchQuery !== '' && <p className="text-white text-2xl text-center flex justify-center items-center p-6">Cant find movie that you are looking for.</p>}
   {showModal && <Modal movie={selectedMovie} showModal={showModal} hideModal={hideModal} />}
 </div>
 </>
